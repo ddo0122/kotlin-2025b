86bdd94cd6950b78ebf8f7ec089f454bc6464ac1
ddo0122 <kjh27256901@gmail.com>
2025-09-12 16:18:16 +0900

    week02 : functions  v0.1

--- KotlinBasics/app/src/main/java/com/myapplication/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/myapplication/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
             }
         }
         week02Variables()
+        week02Functions()
     }
 }
 
@@ -55,6 +56,31 @@ fun week02Variables() {
 
     println("Age : $age, Height : $height, Student : $isStudent")
 
+}
+
+fun week02Functions() {
+    /*println("Week02 Functions");
+
+    fun greet(name: String) = "Hello, $name!"
+
+    println(greet("Android Developer"))*/
+
+    println(" == Kotlin Functions ==")
+
+    fun greet(name : String) : String {
+        return "Hello, $name"
+    }
+
+    fun add(a : Int, b : Int) = a + b
+
+    fun introduce(name : String, age  : Int = 19) {
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum : ${add(5, -71)}")
+    introduce("Park")
+    introduce("Kim", 24)
 }
 
 @Composable
