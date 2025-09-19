078a5fdccb54a34fbbda88ffac47c7746521a70e
ddo0122 <kjh27256901@gmail.com>
2025-09-19 15:36:04 +0900

    week03 : collections v0.2 classes v0.2

--- KotlinBasics/app/src/main/java/com/myapplication/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/myapplication/MainActivity.kt
@@ -30,8 +30,8 @@ class MainActivity : ComponentActivity() {
         }
         //week02Variables()
         //week02Functions()
-        //week03Classes()
-        week03Collections()
+        week03Classes()
+        //week03Collections()
     }
 }
 
@@ -39,18 +39,56 @@ private fun week03Collections() {
     Log.d("KotlinWeek03" ,"week03 == Kotlin Collections ==")
 
     val fruits = listOf("apple", "banana", "orange")
+    val mutableFruits = mutableListOf("kiwi", "watermelon")
     //fruits.add("kiwi")
-    Log.d("KotlinWeek03" ,"Fruits : $fruits")
 
+    mutableFruits.add("banana")
+    Log.d("KotlinWeek03" ,"Fruits : $fruits")
+    Log.d("KotlinWeek03" ,"Mutalble Fruits : $mutableFruits")
+
+    val scores = mapOf("Kim" to 97, "Park" to 100, "Lee" to 99)
+    Log.d("KotlinWeek03" ,"Scores : $scores")
+    
     for (fruit in fruits) {
         Log.d("KotlinWeek03" ,"Fruits : $fruit")
     }
+    scores.forEach{(name, score) -> Log.d("KotlinWeek03", "$name scroed: $score")}
 }
 
 private fun week03Classes() {
-    println("== Kotlin Classes ==")
+    Log.d("KotlinWeek03", "== Kotlin Classes ==")
 
-    class Student {
+    class Person(val name: String, var age: Int) {
+        fun introduce() {
+            Log.d("KotlinWeek03", "안녕하세요. $name ($age 세)입니다.")
+        }
+
+        fun birthday() {
+            age++
+            Log.d("KotlinWeek03", "$name 의 생일! 이제 $age 세...")
+
+        }
+    }
+
+    val person1 = Person("홍길동", 31)
+    person1.introduce()
+    person1.birthday()
+
+    class Animal(var species: String) {
+        var weight: Double = 0.0
+        constructor(species: String, weight: Double) : this(species) {
+            this.weight = weight
+            Log.d("KotlinWeek03", "$species 의 무게:  $weight kg")
+        }
+        fun makeSound() {
+            Log.d("KotlinWeek03", "$species 가 소리를 냅니다. ")
+        }
+    }
+
+    val puppy = Animal("강아지", 6.5)
+    puppy.makeSound()
+
+    /*class Student {
         var name: String = ""
         var age: Int = 0;
 
@@ -69,7 +107,7 @@ private fun week03Classes() {
     var person2 = Person("Lee", 22)
 
     println("Person1 : $person1")
-    println("Equal? : ${person1 == person2}")
+    println("Equal? : ${person1 == person2}")*/
 
 }
 
