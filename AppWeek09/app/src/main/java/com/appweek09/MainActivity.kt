64026c4db7be41ed2668e99f5e88741b8d91659d
ddo0122 <kjh7256901@gmail.com>
2025-11-07 16:02:12 +0900

    week09 : Student List App v1.0

--- AppWeek09/app/src/main/java/com/appweek09/MainActivity.kt
+++ AppWeek09/app/src/main/java/com/appweek09/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.appweek09
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.appweek09.data.Student
+import com.appweek09.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var studentList: ArrayList<Student>
+    private lateinit var studentAdapter: StudentAdapter
+
+    companion object{
+        private const val TAG = "KotlinWeek09App"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(TAG, "onCreate: AppWeek09 RecyclerView started")
+
+//        setUpViews()
+//        setUpRecyclerView()
+//        setUpListeners()
+//        editInitialData()
+
+
         }
     }
-}
