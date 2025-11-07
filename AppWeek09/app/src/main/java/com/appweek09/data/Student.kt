81db9bf84cc106a517acd45f747e266b0efb0623
ddo0122 <kjh7256901@gmail.com>
2025-11-07 14:31:35 +0900

    week09 : Student List App v0.4(Student.kt)

---
+++ AppWeek09/app/src/main/java/com/appweek09/data/Student.kt
@@ -0,0 +1,21 @@
+package com.appweek09.data
+
+import java.util.Date
+import java.util.UUID
+
+data class Student(
+    val name: String,
+    val id: String =  UUID.randomUUID().toString(),
+    val department: String = "Computer Science",
+    val grade: String = "2nd year",
+    val email: String = "",
+    val addedDate: Date = Date()
+) {
+    override fun toString(): String = name
+
+
+//    override fun toString(): String {
+//        return
+//    }
+
+}
