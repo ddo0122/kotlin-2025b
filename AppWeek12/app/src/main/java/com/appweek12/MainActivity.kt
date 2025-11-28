5c560c4c667b7529208072265b16bfc7d2b6935d
ddo0122 <kjh27256901@gmail.com>
2025-11-28 15:26:03 +0900

    week12: Counter App v0.5(MVVM : LiveData)

--- AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
+++ AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
@@ -3,6 +3,7 @@ package com.appweek12
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,7 +11,8 @@ import com.appweek12.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var count = 0;
+//    private var count = 0;
+    private val viewModel: CounterViewMdoel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,47 +20,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(savedInstanceState != null) {
-            count = savedInstanceState.getInt("count", 0)
-        }
-
+        setupObservers()
         setupListeners()
-        updateCountDisplay()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", count)
+    
+
+    private fun setupObservers() {
+        viewModel.count.observe(this) {
+            count -> binding.textViewCount.text = count.toString();
+
+            when {
+                count > 0 -> binding.textViewCount.setTextColor(Color.BLUE)
+                count < 0 -> binding.textViewCount.setTextColor(Color.RED)
+                else -> binding.textViewCount.setTextColor(Color.BLACK)
+            }
+        }
     }
 
     private fun setupListeners() {
         binding.buttonPlus.setOnClickListener {
-            count++;
-            updateCountDisplay();
+            viewModel.increment()
         }
         binding.buttonMinus.setOnClickListener {
-            count--;
-            updateCountDisplay();
+            viewModel.decrement()
         }
         binding.buttonReset.setOnClickListener {
-            count = 0;
-            updateCountDisplay();
+            viewModel.reset()
         }
         binding.buttonPlus10.setOnClickListener {
-            count += 10;
-            updateCountDisplay();
+            viewModel.incrementBy10()
         }
     }
 
-    private fun updateCountDisplay() {
-        binding.textViewCount.text = count.toString();
-
-        when {
-            count > 0 -> binding.textViewCount.setTextColor(Color.BLUE)
-            count < 0 -> binding.textViewCount.setTextColor(Color.RED)
-            else -> binding.textViewCount.setTextColor(Color.BLACK)
-        }
-    }
 
 
 }
