b626efdf96ad2eb713698533b23301f28939f1f0
ddo0122 <kjh27256901@gmail.com>
2025-11-28 14:38:32 +0900

    AppWeek12: Counter App v0.3 (Backend)

---
+++ AppWeek12/app/src/main/java/com/appweek12/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.appweek12
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.appweek12.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonPlus.setOnClickListener {
+            count++;
+            updateCountDisplay();
+        }
+        binding.buttonMinus.setOnClickListener {
+            count--;
+            updateCountDisplay();
+        }
+        binding.buttonReset.setOnClickListener {
+            count = 0;
+            updateCountDisplay();
+        }
+        binding.buttonPlus10.setOnClickListener {
+            count += 10;
+            updateCountDisplay();
+        }
+    }
+
+    private fun updateCountDisplay() {
+        binding.textViewCount.text = count.toString();
+
+        when {
+            count > 0 -> binding.textViewCount.setTextColor(Color.BLUE)
+            count < 0 -> binding.textViewCount.setTextColor(Color.RED)
+            else -> binding.textViewCount.setTextColor(Color.BLACK)
+        }
+    }
+
+
+}
